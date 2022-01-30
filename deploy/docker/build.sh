88c6165c28449f246667e67cc245a605d2d55aec
Sean Nelson <hyperkineticnerd@gmail.com>
2022-01-30 07:13:56 -0700

    UBI8 defectdojo containers work with podman and podman-compose

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -8,9 +8,9 @@ CONTEXT=.
 podman build ${CONTEXT} -f ${DOCKERFILE} -t ${IMAGE}:${TAG}
 
 
-DOCKERFILE=deploy/docker/Containerfile.nginx
-IMAGE=quay.io/hyperkineticnerd/defectdojo-nginx
-TAG=latest
-CONTEXT=.
+# DOCKERFILE=deploy/docker/Containerfile.nginx
+# IMAGE=quay.io/hyperkineticnerd/defectdojo-nginx
+# TAG=latest
+# CONTEXT=.
 
-podman build ${CONTEXT} -f ${DOCKERFILE} -t ${IMAGE}:${TAG}
+# podman build ${CONTEXT} -f ${DOCKERFILE} -t ${IMAGE}:${TAG}
