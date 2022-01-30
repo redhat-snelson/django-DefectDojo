850f4e891d531f75b17ac4f8b64c8f011b6c0864
Sean Nelson <hyperkineticnerd@gmail.com>
2022-01-29 23:13:33 -0700

    Containerfiles for defectdojo should be working

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-DOCKERFILE=deploy/docker
-IMAGE=quay.io/hyperkineticnerd/defectdojo
+DOCKERFILE=deploy/docker/Containerfile.django
+IMAGE=quay.io/hyperkineticnerd/defectdojo-django
 TAG=latest
+CONTEXT=.
 
-podman build ${DOCKERFILE} -t ${IMAGE}:${TAG}
+podman build ${CONTEXT} -f ${DOCKERFILE} -t ${IMAGE}:${TAG}
+
+
+DOCKERFILE=deploy/docker/Containerfile.nginx
+IMAGE=quay.io/hyperkineticnerd/defectdojo-nginx
+TAG=latest
+CONTEXT=.
+
+podman build ${CONTEXT} -f ${DOCKERFILE} -t ${IMAGE}:${TAG}
